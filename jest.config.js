d4056d879e9d9ddbaa22ed45c3c66eaed007f9ec
Alex Sobolev <alex.sobolev1@gmail.com>
2020-03-02 17:10:11 +0300

    Update jest config

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,25 @@
+const { pathsToModuleNameMapper } = require('ts-jest/utils');
+const { compilerOptions } = require('./tsconfig');
+
 module.exports = {
   preset: 'ts-jest',
-  testEnvironment: 'node'
+  testEnvironment: 'node',
+  rootDir: '.',
+  testMatch: ['<rootDir>/src/**/*.(spec|test).ts?(x)'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/**/*.d.ts',
+    '!<rootDir>/src/**/*.spec.ts?(x)'
+  ],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
+  cacheDirectory: '<rootDir>/node_modules/.cache/jest',
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+    prefix: '<rootDir>/'
+  }),
+  globals: {
+    'ts-jest': {
+      tsConfig: 'tsconfig.cjs.json',
+      isolatedModules: true
+    }
+  }
 };
